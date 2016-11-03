831e2f769dcdd020e723f3c0963da9f9f27a949d
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-11-03 13:10:10 +0100

    Moved from chrome.processes API to chrome.tabs
    The chrome.processes API is currently dev-only and is therefore not
    preactically useable.
    
    Using the chrome.tabs API requires more code and CPU-cycles at runtime.
    It further gives less control/insight in where/what has caused a
    problem. It does allow for almost equal fucntionality, so for now it is
    considered good enough.

--- javascript/background.js
+++ javascript/background.js
@@ -52,7 +52,7 @@
 // chrome.tabs.reload(integer tabId, object reloadProperties, function callback)
 //   tabId (integer optional): The ID of the tab to reload;
 //         defaults to the selected tab of the current window.
-//   reloadProperties (object optional): bypassCache (boolean	optional): Whether
+//   reloadProperties (object optional): bypassCache (boolean optional): Whether
 //         using any local cache. Default is false.
 //   callback (function optional): If you specify the callback parameter,
 //         it should be a function that looks like this: function() {...};
@@ -60,109 +60,135 @@
 // For the use case of reloading a kiosk app or tabs that the user is using
 // the default of calling chrome.tabs.reload() is best
 
-// Add listener to onExited event that is fired when a tab or
-// underlying thread exits
-// Parameters:
-// exitType (integer): The type of exit that occurred for the process -
-// normal, abnormal, killed, crashed. Only available for renderer processes.
-// exitCode ( optional integer ): The exit code if the process exited abnormally.
-// Only available for renderer processes.
-chrome.processes.onExited.addListener(function (processId, exitType, exitCode) {
-  console.log("chrome.processes.onExited: processId=" + processId + " exitType=" + exitType + " exitCode=" + exitCode);
+var tabSuccessCount = {}; // store of succesful probe calls
+// Interval between checks, do not make this too short (
+// less than 1 second seems unwise, better no less than
+// 2 seconds) since it is also used to determine if a
+// tab is unresponsive
+var checkInterval = 5000; // in milliseconds
+var tabsChecked = {}; // store for arrays of tab-ids that were checked
+var checkIndex = 0; // index of current array of checked tab-ids
+var nrTabs = 0; // current number of tabs
 
-  if(exitType !== 0){
-    console.log("chrome.processes.onExited:: reloading tab.");
-    chrome.tabs.reload();
-  }
-});
-
-// Add listener to onUnresponsive event that is fired when a tab or
-// underlying thread becomes unresponsive
-// Parameters:
-// process.type enum: type of process -
-// "browser", "renderer", "extension", "notification", "plugin", "worker",
-// "nacl", "utility", "gpu", "other"
-chrome.processes.onUnresponsive.addListener(function (process){
-  console.log("chrome.processes.onUnresponsive: id=" + process.id + " os procid=" + process.osProcessId + " type=" + process.type);
-
-  if(process.type === "browser" || process.type === "renderer"){
-    chrome.processes.terminate(process.id, function(didTerminate) {
-      console.log("chrome.processes.terminate: process termination succeeded: " + didTerminate.toString() + ", reloading tab.");
-      chrome.tabs.reload();
-    });
-  }
-});
-
-
-
-/*
-  The following code is only needed when a specific tabs needs to
-  be reloaded
-  NOTE: this code is not complete
-
-var tab_id;
-var tabToProcess = {};
-var processToTab = {};
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    tab_id = tab.id;
-    console.log("onClicked.addListener: tab_id=" + tab_id);
-    chrome.processes.getProcessIdForTab(tab.id, function (processId) {
-      console.log("getProcessIdForTab: tab.id=" + tab.id);
-      if (!isNaN(parseFloat(processId)) && isFinite(processId)) {
-        // chrome.processes.terminate(processId, function(didTerminate) {
-        //   console.log("onClicked.addListener.getProcessIdForTab chrome.processes.terminate(: didTerminate" + toString(didTerminate));
-        //   // chrome.processes.terminate
-        // });
+function reloadTabIfNeeded(tab) {
+  return function(result) {
+    if (tabCrashed()) {
+      console.log("Crashed tab: title=" + (tab.title || "") + " id=" + tab.id +
+                  " index=" + tab.index.toString() + " windowId=" + tab.windowId.toString() +
+                  " sessionId=" + (tab.sessionId || "").toString() +
+                  " highlighted=" + tab.highlighted.toString() + " active=" + tab.active.toString());
+      if (tabShouldBeReloaded(tab)) {
+        console.log("Reload tab:" + tab.id.toString());
+        chrome.tabs.reload(tab.id);
       }
-    });
-});
+    } else {
+      registerSuccessfulNoOp(tab);
+    }
+    tabsChecked[checkIndex].push(tab.id);
+  };
+}
 
-function getTabAndProcessIds(tabs) {
-  var tabsLength = 0;
-  var tabId = 0;
+function tabShouldBeReloaded(tab) {
+  // Reload if at least one sucessful no-op has occurred.
+  // This might be too causious but ensures the tab was working
+  // before it crashed
+  return tabSuccessCount[tab.id] > 0;
+}
 
-  if (tabs !== null && tabs !== undefined && tabs.length > 0) {
-    tabsLength = tabs.length;
-    console.log("getTabsInfo: #Tabs=" + tabsLength);
-    for (var i = 0; i < tabsLength; i += 1) {
-      aTab = tabs[i];
-      chrome.processes.getProcessIdForTab(aTab.id, function(processId) {
-        if (tabToProcess[aTab.id.toString()] === undefined || (tabToProcess[aTab.id.toString()] !== undefined && tabToProcess[aTab.id.toString()] !== processId)) {
-          tabToProcess[aTab.id.toString()] = processId;
-        }
-        if (processToTab[processId.toString()] === undefined || (tabToProcess[processId.toString()] !== undefined && processToTab[processId.toString()] !== aTab.id)) {
-          processToTab[processId.toString()] = aTab.id;
-        }
-      });
+function registerSuccessfulNoOp(tab) {
+  if ((tabSuccessCount[tab.id] || null) === null) {
+    tabSuccessCount[tab.id] = 0;
+  }
+  tabSuccessCount[tab.id] += 1;
+}
+
+function tabCrashed() {
+  // The crux of finding a crashed tab:
+  // If an operation (even a no-op) is executed on a crashed tab an error
+  // is reported which is available as the chrome.runtime.lastError
+  // The error incorrectly reports the tab was closed instead of the fact that the
+  // tab does not respond.
+  return chrome.runtime.lastError && chrome.runtime.lastError.message === "The tab was closed.";
+}
+
+function checkTab(thisTab) {
+  if (relevantTab(thisTab)) {
+    // Perform a no-op as a probe to find if the tab reponds
+    chrome.tabs.executeScript(thisTab.id, {
+      // To find crashed tabs probing with a no-op is enough
+      // code: "null;"
+      // To find unresponsive tabs probing with some operation
+      // that takes CPU-cycles is needed
+      code: "1 + 1;"
+    }, reloadTabIfNeeded(thisTab));
+  }
+}
+
+function relevantTab(tab){
+  // Only check tabs that have finished loading
+  // and that use the http or https protocol.
+  // This ignores tabs like chrome://...
+  // return tab.url.substring(0, 4) == "http" && tab.status == "complete";
+  // This makes testing this extension more difficult, to test use
+  // the line below
+  return tab.status == "complete";
+}
+
+function reloadUnresposiveTabs(index, nrTabs, tabs) {
+  if (nrTabs === tabs.length && nrTabs > tabsChecked[index].length) {
+    var nrTabsToFind = nrTabs - tabsChecked[index].length;
+    console.log("Found " + nrTabsToFind.toString() + " unresponsive tabs");
+    for (var j = 0; j < nrTabs && nrTabsToFind > 0; j += 1) {
+      if (tabsChecked[index].indexOf(tabs[j].id) == -1) {
+        console.log("Reload unresponsive tab:" + tabs[j].id.toString());
+        // Reloading an unresponsive tab does not work
+        // chrome.tabs.reload(tabs[j].id);
+        // Therefore a new tab is created with the url of the old tab
+        // and the unresponsive tab is removed.
+        // Setting the new tab as active is mainly aimed at kiosk-like applications
+        chrome.tabs.create({url: tabs[j].url, active: true}, function(tab){
+          console.log("Created new tab: id=" + tab.id.toString() + " title=" + (tab.title || "") + " url=" + (tab.url || ""));
+        });
+        chrome.tabs.remove(tabs[j].id, function(){
+          console.log("Removed unresponsive tab:" + tabs[j].id.toString());
+        });
+        nrTabsToFind -= 1;
+      }
     }
   }
 }
 
-function getTabsInfo(tabs) {
-  var tabsLength = 0;
-  var tabId = 0;
+function checkTabs(tabs) {
+  // check for unresponsive tabs by checking the results of
+  // the previous round of checkTab calls
+  // NOTE: it is assumed all tabs have been checked (all callbacks
+  // initiated in the previous checkTabs call have ended (apart
+  // form the ones that were done on unresponsive tabs)). The
+  // checkInterval has to be long enough to make this a "certainty"
+  if (nrTabs > 0) {
+    reloadUnresposiveTabs(checkIndex, nrTabs, tabs);
+  }
 
-  if (tabs !== null && tabs !== undefined && tabs.length > 0) {
-    tabsLength = tabs.length;
-    for (var i = 0; i < tabsLength; i += 1) {
-      aTab = tabs[i];
-      console.log("getTabsInfo: i=" + i + " tab.id=" + aTab.id);
-      tabId = chrome.processes.getProcessIdForTab(aTab.id, function(processId) {
-        console.log("getTabsInfo: processId=" + processId);
-      });
-      console.log("getTabsInfo: tabId=" + tabId);
-    }
-  } else {
-    console.log("getTabsInfo: tabs null/undefined/empty");
+  // roll the checkIndex around after 10 iterations
+  checkIndex = checkIndex > 9 ? 0 : (checkIndex + 1);
+  nrTabs = tabs.length;
+  tabsChecked[checkIndex] = [];
+  for (var i = 0; i < tabs.length; i += 1) {
+    checkTab(tabs[i]);
   }
 }
 
-// Check now and once a minute on all tabs to have current processIds
-chrome.tabs.query({}, getTabAndProcessIds);
-var tabAndProcessPID = setInterval(function() {
-    chrome.tabs.query({}, getTabAndProcessIds);
-}, 60000);
+// Reset the count for tabs that are closed or that change
+function tabChanged(tabId, changeInfo, tab) {
+  console.log("Resetting Stats for tab: id=" + tabId.toString() + " title=" +
+              (tab !== undefined ? tab.title : ""));
+  tabSuccessCount[tabId] = 0;
+}
 
-chrome.tabs.query({}, getTabsInfo);
-*/
+setInterval(function() {
+  chrome.tabs.query({}, checkTabs);
+}, checkInterval);
+
+// If the tab reloads, reset stats
+chrome.tabs.onUpdated.addListener(tabChanged);
+chrome.tabs.onRemoved.addListener(tabChanged);
